7993fb545be97b537fc5c43a6ea7d8687ea21719
Tr7nCatc4M3 <114768946+Tr7nCatc4M3@users.noreply.github.com>
2022-10-25 13:53:25 +0000

    Created new Vector3 class

--- Vector3.h
+++ Vector3.h
@@ -5,101 +5,47 @@
 #define PI	3.14159265358979323846264338327950288419716939937510
 
 /// <summary>
-/// Vector3 operations using x, y, z
-/// </summary>
+/// Defining Vector3 operators and floats
+/// <summary>
 class Vector3 {
 public:
-	Vector3() {
-		x = y = z = 0.f;
+	Vector3() : x(0.f), y(0.f), z(0.f) {
 	}
 
-	Vector3(float cx, float cy, float cz) {
-		x = cx;
-		y = cy;
-		z = cz;
+	Vector3(float _x, float _y, float _z) : x(_x), y(_y), z(_z) {
 	}
 
-	float x, y, z;
+	~Vector3() {
+	}
+	float x;
+	float y;
+	float z;
 
-	Vector3 operator+(const Vector3& input) const {
-		return Vector3{ x + input.x, y + input.y, z + input.z };
+	inline float Dot(Vector3 v) {
+		return x * v.x + y * v.y + z * v.z;
 	}
 
-	Vector3 operator-(const Vector3& input) const {
-		return Vector3{ x - input.x, y - input.y, z - input.z };
+	inline float Distance(Vector3 v) {
+		return float(sqrtf(powf(v.x - x, 2.0) + powf(v.y - y, 2.0) + powf(v.z - z, 2.0)));
 	}
 
-	Vector3 operator/(const Vector3& input) const {
-		return Vector3{ x / input.x, y / input.y, z / input.z };
+	inline double Length() {
+		return sqrt(x * x + y * y + z * z);
 	}
 
-	Vector3 operator*(const Vector3& input) const {
-		return Vector3{ x * input.x, y * input.y, z * input.z };
+	inline FRotator ToFRotator() {
+		FRotator rot = FRotator();
+		float RADPI = (float)(180 / PI);
+		rot.Yaw = (float)atan2(y, x) * RADPI;
+		rot.Pitch = (float)atan2(z, sqrt((x * x) + (y * y))) * RADPI;
+		rot.Roll = 0;
+		return rot;
 	}
-
-	Vector3& operator-=(const Vector3& v) {
-		x -= v.x;
-		y -= v.y;
-		z -= v.z;
-		return *this;
+	Vector3 operator+(Vector3 v) {
+		return Vector3(x + v.x, y + v.y, z + v.z);
 	}
-
-	Vector3& operator*=(float input) {
-		x *= input;
-		y *= input;
-		z *= input;
-		return *this;
+	Vector3 operator-(Vector3 v) {
+		return Vector3(x - v.x, y - v.y, z - v.z);
 	}
-
-	Vector3& operator/=(float input) {
-		x /= input;
-		y /= input;
-		z /= input;
-		return *this;
-	}
-
-	bool operator==(const Vector3& input) const {
-		return x == input.x && y == input.y && z == input.z;
-	}
-
-	// These are not needed but I intend to use later on
-	void make_absolute() {
-		x = std::abs(x);
-		y = std::abs(y);
-		z = std::abs(z);
-	}
-
-	float length_sqr() const {
-		return (x * x) + (y * y) + (z * z);
-	}
-
-	float length() const {
-		return std::sqrt(length_sqr());
-	}
-
-	float length_2d() const {
-		return std::sqrt((x * x) + (y * y));
-	}
-
-	Vector3 normalized() const {
-		return { x / length(), y / length(), z / length() };
-	}
-
-	float dot_product(Vector3 input) const {
-		return (x * input.x) + (y * input.y) + (z * input.z);
-	}
-
-	float distance(Vector3 input) const {
-		return (*this - input).length();
-	}
-
-	float distance_2d(Vector3 input) const {
-		return (*this - input).length_2d();
-	}
-
-	// No clamping
-
-	bool empty() const {
-		return x == 0.f && y == 0.f && z == 0.f;
-	}
+	Vector3 operator*(float flNum) { return Vector3(x * flNum, y * flNum, z * flNum); }
 };
